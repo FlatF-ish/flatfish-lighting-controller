27d6238abd50e878fd26b6854f022e7d95e127e7
Joshua <psyjr4@nottingham.ac.uk>
2019-11-30 17:31:20 +0000

    Added some nicer styles and local status changes for sexy smoothness

--- public/js/lighting.js
+++ public/js/lighting.js
@@ -9,31 +9,40 @@ function turnOn() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://flatfish.online:49161/on", true);
     xhttp.send();
-    // getStatus();
+    styleForOn();
 }
 
 function turnOff() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://flatfish.online:49161/off", true);
     xhttp.send();
-    // getStatus();
+    styleForOff();
+}
+
+function styleForOn() {
+    document.getElementById('off-state').classList.add('inactive');
+    document.getElementById('off-state').classList.remove('off');
+    document.getElementById('on-state').classList.remove('inactive');
+    document.getElementById('on-state').classList.add('on');
+    document.getElementById('js-status').innerHTML = "on"
+}
+
+function styleForOff() {
+    document.getElementById('on-state').classList.add('inactive');
+    document.getElementById('on-state').classList.remove('on');
+    document.getElementById('off-state').classList.add('off');
+    document.getElementById('off-state').classList.remove('inactive');
+    document.getElementById('js-status').innerHTML = "off"
 }
 
 function getStatus() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
-            document.getElementById('js-status').innerHTML = this.responseText;
             if(this.responseText === 'on') {
-                document.getElementById('off-state').classList.add('inactive');
-                document.getElementById('off-state').classList.remove('off');
-                document.getElementById('on-state').classList.remove('inactive');
-                document.getElementById('on-state').classList.add('on');
+                styleForOn();
             } else if (this.responseText === 'off') {
-                document.getElementById('on-state').classList.add('inactive');
-                document.getElementById('on-state').classList.remove('on');
-                document.getElementById('off-state').classList.add('off');
-                document.getElementById('off-state').classList.remove('inactive');
+                styleForOff();
             }
         }
     }

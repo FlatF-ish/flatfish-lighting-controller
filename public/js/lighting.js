0ee2f95f254227dd47fdf8c5337b90690ed96e31
Joshua <psyjr4@nottingham.ac.uk>
2019-11-30 19:16:56 +0000

    Disable buttons while waiting for response

--- public/js/lighting.js
+++ public/js/lighting.js
@@ -9,14 +9,14 @@ function turnOn() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://flatfish.online:49161/on", true);
     xhttp.send();
-    styleForOn();
+    waitForResponse();
 }
 
 function turnOff() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://flatfish.online:49161/off", true);
     xhttp.send();
-    styleForOff();
+    waitForResponse();
 }
 
 function styleForOn() {
@@ -35,14 +35,42 @@ function styleForOff() {
     document.getElementById('js-status').innerHTML = "off";
 }
 
+function waitForResponse() {
+    document.getElementById('on-state').disabled = true;
+    document.getElementById('off-state').disabled = true;
+    loadAnimation();
+}
+
+function onResponseReceived() {
+    document.getElementById('on-state').disabled = false;
+    document.getElementById('off-state').disabled = false;
+    removeAnimation();
+}
+
+function loadAnimation() {
+    document.getElementById('light-one').classList.add('light-one');
+    document.getElementById('light-two').classList.add('light-two');
+    document.getElementById('light-three').classList.add('light-three');
+    document.getElementById('light-four').classList.add('light-four');
+}
+
+function removeAnimation() {
+    document.getElementById('light-one').classList.remove('light-one');
+    document.getElementById('light-two').classList.remove('light-two');
+    document.getElementById('light-three').classList.remove('light-three');
+    document.getElementById('light-four').classList.remove('light-four');
+}
+
 function getStatus() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
             if(this.responseText === 'on') {
                 styleForOn();
+                onResponseReceived();
             } else if (this.responseText === 'off') {
                 styleForOff();
+                onResponseReceived();
             }
         }
     }

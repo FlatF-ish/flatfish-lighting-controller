c43afc6ace62921e98ca9f32e1db32c60dd6c016
Joshua <psyjr4@nottingham.ac.uk>
2019-12-01 00:12:47 +0000

    Refactored toggle

--- tplink.js
+++ tplink.js
@@ -2,21 +2,58 @@ const myUser = process.env.TP_LINK_USER;
 const myPass = process.env.TP_LINK_PASSWORD;
 const { login } = require("tplink-cloud-api")
 
-function toggle() {
-    return new Promise(function(accept, reject){
-        login(myUser, myPass).then( tplink => {
-            tplink.getDeviceList().then(deviceList => {
-                tplink.getHS100('Christmas Lights').toggle().then(something => {
-                    let myPlug = tplink.getHS100("Christmas Lights")
-                    myPlug.getRelayState().then(state => {
-                        console.log(state);
-                        accept(state);
-                    })
-                })
-            })
-        });
+var loggedInUser;
+var devices;
+var plug;
+var ready;
+
+
+function logUserIn() {
+    return login(myUser, myPass)
+}
+
+function getDevices() {
+    return loggedInUser.getDeviceList();
+}
+
+function getPlug() {
+    return loggedInUser.getHS100("Christmas Lights");
+}
+
+
+logUserIn().then(user => {
+    loggedInUser = user;
+    return getDevices();
+}).then(deviceList => {
+    devices = deviceList;
+    return getPlug();
+}).then(myPlug => {
+    plug = myPlug;
+    ready = true;
+})
+
+
+
+async function toggle() {
+    plug.toggle().then(() => {
+        return plug.getRelayState()
     });
 }
+
+//     return new Promise(function(accept, reject){
+//         login(myUser, myPass).then( tplink => {
+//             tplink.getDeviceList().then(deviceList => {
+//                 tplink.getHS100('Christmas Lights').toggle().then(something => {
+//                     let myPlug = tplink.getHS100("Christmas Lights")
+//                     myPlug.getRelayState().then(state => {
+//                         console.log(state);
+//                         accept(state);
+//                     })
+//                 })
+//             })
+//         });
+//     });
+// }
 
 function turnOn() {
     return new Promise(function(accept, reject){

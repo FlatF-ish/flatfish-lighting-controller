5b556216523544eac2b032b1ce8eae41af5f7e27
Samuel Williams <williamssamuel1999@gmail.com>
2019-12-01 01:32:14 +0000

    Cleaned with EsLint

--- index.js
+++ index.js
@@ -1,80 +1,79 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const tplink = require('./tplink.js');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const tplink = require("./tplink.js");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    var file = fs.readFileSync(process.cwd()+'/lighting.html').toString();
-    res.send(file);
+app.get("/", (req, res) => {
+	var file = fs.readFileSync(process.cwd() + "/lighting.html").toString();
+	res.send(file);
 });
 
-app.get('/on', (req, res) => {
-    tplink.turnOn().then( () => {
-        console.log("On");
-        res.send("On");
-    });
+app.get("/on", (req, res) => {
+	tplink.turnOn().then(() => {
+		console.log("On");
+		res.send("On");
+	});
 });
 
-app.get('/toggle', (req, res) => {
-    tplink.toggle().then(() => {
-        console.log("toggled");
-        res.send("Toggled");
-    });
+app.get("/toggle", (req, res) => {
+	tplink.toggle().then(() => {
+		console.log("toggled");
+		res.send("Toggled");
+	});
 });
 
-app.get('/off', (req, res) => {
-    tplink.turnOff().then( () => {
-        console.log("Off");
-        res.send("Off");
-    });
+app.get("/off", (req, res) => {
+	tplink.turnOff().then(() => {
+		console.log("Off");
+		res.send("Off");
+	});
 });
 
-app.get('/status', (req, res) => {
-    tplink.getStatus().then((status) => {
-        res.send(`${status}`);
-    })
+app.get("/status", (req, res) => {
+	tplink.getStatus().then((status) => {
+		res.send(`${status}`);
+	});
 });
 
-app.use('/api', bodyParser.json());
-app.use('/favicon.ico', express.static('./public/images/bulb.ico'));
+app.use("/api", bodyParser.json());
+app.use("/favicon.ico", express.static("./public/images/bulb.ico"));
 
-app.post('/api', (req, res) => {
-    switch(req.body.type) {
-        case "on": 
-            tplink.turnOn().then(() => { 
-                res.status(200).send("Lights turned on"); 
-            });
-            break;
-        case "off": 
-            tplink.turnOff().then(() => { 
-                res.status(200).send("Lights turned off"); 
-            });
-            break;
-        case "toggle": 
-            tplink.toggle()
-                .then(() => tplink.getStatus())
-                .then((status) => { 
-                    res.status(200).send(`Lights turned ${status}`); 
-            });
-            break;
-        case "status": 
-            tplink.getStatus().then(status => { 
-                res.status(200).send("Lights status: " + status); 
-            });
-            break;
-        default:
-            res.status(400).send("Invalid type");
-            break;
-    }
+app.post("/api", (req, res) => {
+	switch (req.body.type) {
+		case "on":
+			tplink.turnOn().then(() => {
+				res.status(200).send("Lights turned on");
+			});
+			break;
+		case "off":
+			tplink.turnOff().then(() => {
+				res.status(200).send("Lights turned off");
+			});
+			break;
+		case "toggle":
+			tplink.toggle()
+				.then(() => tplink.getStatus())
+				.then((status) => {
+					res.status(200).send(`Lights turned ${status}`);
+				});
+			break;
+		case "status":
+			tplink.getStatus().then(status => {
+				res.status(200).send("Lights status: " + status);
+			});
+			break;
+		default:
+			res.status(400).send("Invalid type");
+			break;
+	}
 });
 
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000!');
-})
+	console.log("Lighting controller listening on port 8000!");
+});
--- tplink.js
+++ tplink.js
@@ -1,6 +1,6 @@
 const myUser = process.env.TP_LINK_USER;
 const myPass = process.env.TP_LINK_PASSWORD;
-const { login } = require("tplink-cloud-api")
+const { login } = require("tplink-cloud-api");
 
 var loggedInUser;
 var devices;
@@ -8,51 +8,51 @@ var plug;
 var ready = false;
 
 async function logUserIn() {
-    loggedInUser = await login(myUser, myPass)
+	loggedInUser = await login(myUser, myPass);
 }
 
 async function getDevices() {
-    devices = await loggedInUser.getDeviceList();
+	devices = await loggedInUser.getDeviceList();
 }
 
 async function getPlug() {
-    plug = await loggedInUser.getHS100("Christmas Lights");
+	plug = await loggedInUser.getHS100("Christmas Lights");
 }
 
 function checkSetupCompleted() {
-    if(!ready) {
-        throw new Error("Sozza ma boz, not quite ready for ma dudes yet - check back soon");
-    }
+	if (!ready) {
+		throw new Error("Sozza ma boz, not quite ready for ma dudes yet - check back soon");
+	}
 }
 
 function toggle() {
-    checkSetupCompleted();
-    return plug.toggle();
+	checkSetupCompleted();
+	return plug.toggle();
 }
 
 function turnOn() {
-    checkSetupCompleted();
-    return plug.powerOn();
+	checkSetupCompleted();
+	return plug.powerOn();
 }
 
 function turnOff() {
-    checkSetupCompleted();
-    return plug.powerOff();
+	checkSetupCompleted();
+	return plug.powerOff();
 }
 
 async function getStatus() {
-    checkSetupCompleted();
-    var status = await plug.isOn();
-    return status ? "on" : "off";
+	checkSetupCompleted();
+	var status = await plug.isOn();
+	return status ? "on" : "off";
 }
 
 async function setup() {
-    await logUserIn();
-    await getDevices();
-    await getPlug();
-    ready = true;
+	await logUserIn();
+	await getDevices();
+	await getPlug();
+	ready = true;
 }
 
 setup();
 
-module.exports = {turnOn: turnOn, turnOff: turnOff, toggle: toggle, getStatus: getStatus}
+module.exports = { turnOn: turnOn, turnOff: turnOff, toggle: toggle, getStatus: getStatus };

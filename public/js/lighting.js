9cef3f9653a9e3dc2ced57e83d91f2d21c97f54d
Samuel Williams <williamssamuel1999@gmail.com>
2019-12-01 01:37:47 +0000

    Cleaned public/js/lighting.js

--- public/js/lighting.js
+++ public/js/lighting.js
@@ -1,113 +1,111 @@
 var disableStatusUpdate = false;
 
-document.addEventListener('readystatechange', event => {
-    if(event.target.readyState === "complete") {
-        getStatus();                    
-        setInterval(getStatus, 1000);
-    }
+document.addEventListener("readystatechange", event => {
+	if (event.target.readyState === "complete") {
+		getStatus();
+		setInterval(getStatus, 1000);
+	}
 });
 
 function turnOn() {
-    disableStatusUpdate = true;
-    waitForResponse();
-    const xhttp = new XMLHttpRequest();
+	disableStatusUpdate = true;
+	waitForResponse();
+	const xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            disableStatusUpdate = false;
-        }
-    }
-    xhttp.open("GET", "http://flatfish.online:49161/on", true);
-    xhttp.send();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			disableStatusUpdate = false;
+		}
+	};
+	xhttp.open("GET", "http://flatfish.online:49161/on", true);
+	xhttp.send();
 }
 
 function turnOff() {
-    disableStatusUpdate = true;
-    waitForResponse();
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            disableStatusUpdate = false;
-        }
-    }
-    xhttp.open("GET", "http://flatfish.online:49161/off", true);
-    xhttp.send();
+	disableStatusUpdate = true;
+	waitForResponse();
+	const xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			disableStatusUpdate = false;
+		}
+	};
+	xhttp.open("GET", "http://flatfish.online:49161/off", true);
+	xhttp.send();
 }
 
 function styleForOn() {
-    document.getElementById('off-state').classList.add('inactive');
-    document.getElementById('off-state').classList.remove('off');
-    document.getElementById('on-state').classList.remove('inactive');
-    document.getElementById('on-state').classList.add('on');
+	document.getElementById("off-state").classList.add("inactive");
+	document.getElementById("off-state").classList.remove("off");
+	document.getElementById("on-state").classList.remove("inactive");
+	document.getElementById("on-state").classList.add("on");
 
-    document.getElementById('js-status').innerHTML = "On";
+	document.getElementById("js-status").innerHTML = "On";
 
-    document.getElementById('lightbulb-icon').classList.remove('lightbulb-icon-off');
-    document.getElementById('lightbulb-icon').classList.add('lightbulb-icon-on');
+	document.getElementById("lightbulb-icon").classList.remove("lightbulb-icon-off");
+	document.getElementById("lightbulb-icon").classList.add("lightbulb-icon-on");
 }
 
 function styleForOff() {
-    document.getElementById('on-state').classList.add('inactive');
-    document.getElementById('on-state').classList.remove('on');
-    document.getElementById('off-state').classList.add('off');
-    document.getElementById('off-state').classList.remove('inactive');
+	document.getElementById("on-state").classList.add("inactive");
+	document.getElementById("on-state").classList.remove("on");
+	document.getElementById("off-state").classList.add("off");
+	document.getElementById("off-state").classList.remove("inactive");
 
-    document.getElementById('js-status').innerHTML = "Off";
+	document.getElementById("js-status").innerHTML = "Off";
 
-    document.getElementById('lightbulb-icon').classList.remove('lightbulb-icon-on');
-    document.getElementById('lightbulb-icon').classList.add('lightbulb-icon-off');
+	document.getElementById("lightbulb-icon").classList.remove("lightbulb-icon-on");
+	document.getElementById("lightbulb-icon").classList.add("lightbulb-icon-off");
 }
 
 function waitForResponse() {
-    document.getElementById('js-status').innerHTML = "Waiting...";
-    document.getElementById('on-state').classList.add('inactive');
-    document.getElementById('off-state').classList.add('inactive');
-    document.getElementById('on-state').classList.add('disabled');
-    document.getElementById('off-state').classList.add('disabled');
-    document.getElementById('on-state').disabled = true;
-    document.getElementById('off-state').disabled = true;
-    loadAnimation();
+	document.getElementById("js-status").innerHTML = "Waiting...";
+	document.getElementById("on-state").classList.add("inactive");
+	document.getElementById("off-state").classList.add("inactive");
+	document.getElementById("on-state").classList.add("disabled");
+	document.getElementById("off-state").classList.add("disabled");
+	document.getElementById("on-state").disabled = true;
+	document.getElementById("off-state").disabled = true;
+	loadAnimation();
 }
 
 function onResponseReceived() {
-    document.getElementById('on-state').classList.remove('inactive');
-    document.getElementById('off-state').classList.remove('inactive');
-    document.getElementById('on-state').classList.remove('disabled');
-    document.getElementById('off-state').classList.remove('disabled');
-    document.getElementById('on-state').disabled = false;
-    document.getElementById('off-state').disabled = false;
-    removeAnimation();
+	document.getElementById("on-state").classList.remove("inactive");
+	document.getElementById("off-state").classList.remove("inactive");
+	document.getElementById("on-state").classList.remove("disabled");
+	document.getElementById("off-state").classList.remove("disabled");
+	document.getElementById("on-state").disabled = false;
+	document.getElementById("off-state").disabled = false;
+	removeAnimation();
 }
 
 function loadAnimation() {
-    document.getElementById('light-one').classList.add('light-one');
-    document.getElementById('light-two').classList.add('light-two');
-    document.getElementById('light-three').classList.add('light-three');
-    document.getElementById('light-four').classList.add('light-four');
+	document.getElementById("light-one").classList.add("light-one");
+	document.getElementById("light-two").classList.add("light-two");
+	document.getElementById("light-three").classList.add("light-three");
+	document.getElementById("light-four").classList.add("light-four");
 }
 
 function removeAnimation() {
-    document.getElementById('light-one').classList.remove('light-one');
-    document.getElementById('light-two').classList.remove('light-two');
-    document.getElementById('light-three').classList.remove('light-three');
-    document.getElementById('light-four').classList.remove('light-four');
+	document.getElementById("light-one").classList.remove("light-one");
+	document.getElementById("light-two").classList.remove("light-two");
+	document.getElementById("light-three").classList.remove("light-three");
+	document.getElementById("light-four").classList.remove("light-four");
 }
 
 function getStatus() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200 && !disableStatusUpdate) {
-            if(this.responseText === 'on') {
-                onResponseReceived();
-                styleForOn();
-            } else if (this.responseText === 'off') {
-                onResponseReceived();
-                styleForOff();
-            }
-        }
-    }
-    xhttp.open("GET", "http://flatfish.online:49161/status", true);
-    xhttp.send();
+	const xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200 && !disableStatusUpdate) {
+			if (this.responseText === "on") {
+				onResponseReceived();
+				styleForOn();
+			} else if (this.responseText === "off") {
+				onResponseReceived();
+				styleForOff();
+			}
+		}
+	};
+	xhttp.open("GET", "http://flatfish.online:49161/status", true);
+	xhttp.send();
 }
-
-
